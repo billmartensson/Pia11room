d5644d953fc5dbd22fb528bf1c279b327ed00091
Bill Mårtensson <bill@magictechnology.se>
2023-03-06 11:31:48 +0100

    pia11

---
+++ app/src/main/java/se/magictechnology/pia11room/MainActivity.kt
@@ -0,0 +1,86 @@
+package se.magictechnology.pia11room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    var peopleadapter = PeopleAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var peoplerv = findViewById<RecyclerView>(R.id.peopleRV)
+
+        peoplerv.adapter = peopleadapter
+        peoplerv.layoutManager = LinearLayoutManager(this)
+
+
+        findViewById<Button>(R.id.addButton).setOnClickListener {
+            val firstname = findViewById<EditText>(R.id.firstnameET).text.toString()
+            val lastname = findViewById<EditText>(R.id.lastnameET).text.toString()
+
+            val nyperson = User(0, firstname, lastname)
+
+
+        }
+
+
+        loadpeople()
+    }
+
+    fun loadpeople() {
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "database-name"
+        ).build()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val userDao = db.userDao()
+
+            val allpeople = userDao.getAll()
+
+            peopleadapter.people = allpeople.toMutableList()
+
+            peopleadapter.notifyDataSetChanged()
+        }
+    }
+
+
+    fun exempelroom() {
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "database-name"
+        ).build()
+
+        CoroutineScope(Dispatchers.IO).launch {
+
+            val userDao = db.userDao()
+
+            //var nyperson = User(0, "Henning", "")
+            //userDao.insertAll(nyperson)
+
+            val users: List<User> = userDao.getNoLastname()
+
+            Log.i("PIA11DEBUG", "ANTAL " + users.size.toString())
+            for (currentuser in users) {
+                Log.i("PIA11DEBUG", currentuser.firstName!!)
+                Log.i("PIA11DEBUG", currentuser.lastName!!)
+                Log.i("PIA11DEBUG", currentuser.uid!!.toString())
+
+            }
+
+        }
+    }
+
+}
---
+++ app/src/main/java/se/magictechnology/pia11room/UserDB.kt
@@ -0,0 +1,37 @@
+package se.magictechnology.pia11room
+
+import androidx.room.*
+
+@Entity
+data class User(
+    @PrimaryKey(autoGenerate = true) val uid: Int,
+    @ColumnInfo(name = "first_name") val firstName: String?,
+    @ColumnInfo(name = "last_name") val lastName: String?
+)
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    fun getAll(): List<User>
+
+    @Query("SELECT * FROM user WHERE last_name = ''")
+    fun getNoLastname(): List<User>
+
+    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
+    fun loadAllByIds(userIds: IntArray): List<User>
+
+    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
+            "last_name LIKE :last LIMIT 1")
+    fun findByName(first: String, last: String): User
+
+    @Insert
+    fun insertAll(vararg users: User)
+
+    @Delete
+    fun delete(user: User)
+}
+
+@Database(entities = [User::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+}
